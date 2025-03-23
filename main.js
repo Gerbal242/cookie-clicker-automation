cf6f6d9cf117816682415cf773732865ae6652a8
Gerbal242 <girgis.seryamon@gmail.com>
2025-03-22 19:09:34 -0500

    updated ignore upgrades

--- main.js
+++ main.js
@@ -17,9 +17,12 @@
 // Configuration
 const CONFIG = {
   CLICK_RATE: 1, // How often to click the cookie (milliseconds) 1 ms
-  PURCHASE_RATE: 30000, // How often to check purchases (milliseconds) 30 seconds
+  PURCHASE_RATE: 10000, // How often to check purchases (milliseconds) 10 seconds
   ASCENTION_TIME_AFTER: 86400, // How often we should ascend, I am doing it after a day per run (seconds) 24 hours
-  EXCLUDED_UPGRADES: new Set([64, 74, 84, 85, 264, 333]), // Upgrades to skip cuz they make weird stuff
+  EXCLUDED_UPGRADES: new Set([
+    64, 74, 84, 85, 181, 182, 183, 184, 185, 209, 274, 281, 323, 327, 328, 331,
+    333, 414, 264, 333,
+  ]), // Upgrades to skip cuz they make weird stuff
   UPGRADE_COST_RATIO: (1 + Math.sqrt(5)) / 2, // TODO: UTILIZE THIS RATIO
 };
 
