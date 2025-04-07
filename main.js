94935856975ce82d8fda0e9877d8ea5a4e175104
Gerges Ibrahim <72173719+Gerbal242@users.noreply.github.com>
2025-04-07 17:26:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@
 const CONFIG = {
   CLICK_RATE: 1, // How often to click the cookie (milliseconds) 1 ms
   PURCHASE_RATE: 60000, // How often to check purchases (milliseconds) 10 seconds
-  ASCENTION_TIME_AFTER: 86400, // How often we should ascend, I am doing it after a day per run (seconds) 24 hours
+  ASCENTION_TIME_AFTER: 43200, // How often we should ascend, I am doing it after 12 hours
   EXCLUDED_UPGRADES: new Set([
     64, 74, 84, 85, 181, 182, 183, 184, 185, 209, 264, 323, 327, 328, 331,
     333, 414,
